c6370d883b3ad0e0b982b4a25bb86e9897917e8a
Jevon Thompson <jevonx@jevdebianmac.tail>
2025-05-11 21:36:13 -0700

    Added comments for import/exports

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 )
 
 func main() {
+	// all variables/functions capitalized within other files
+	// but packages within the same package (in this case "main")
+	// can be used across each other
+	// eg:
 	Hello()
 	Variables()
+
+	// variables & functions from other packages,
+	// need to be explicitly imported (see top)
+	// and used via their package:
 	importMePackage.Wave()
 }
